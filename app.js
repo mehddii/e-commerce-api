a5708f11a1d6f02add2b4f2ea77a8e52287c22da
mehdi <mehdi7bs@gmail.com>
2025-04-14 23:02:34 +0100

    Integrate MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const express = require("express")
+const express = require('express')
 const productsRouter = require('./controllers/products')
 const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+const MONGODB_URI = require('./utils/config').MONGODB_URI
+
 const app = express()
+
+mongoose.set('strictQuery', false)
+console.log('Connecting to', MONGODB_URI)
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('Successfully connected to MongoDB')
+  })
+  .catch(error => {
+    console.error('Error connectiong to MongoDB', error.message)
+  })
 
 app.use(express.json())
 app.use(middleware.requestLogger)
--- utils/config.js
+++ utils/config.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 
 const PORT = process.env.PORT | 3001
 const ENV = process.env.ENV || 'production'
+const MONGODB_URI = process.env.MONGODB_URI
 
 
 module.exports = {
   PORT,
-  ENV
+  ENV,
+  MONGODB_URI
 }
